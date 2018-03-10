a2626e74ad33fc35c120bcec0736010ddd082151
Stephen SooHoo <ss083193@gmail.com>
2018-03-10 00:51:08 -0600

    fix formatting

--- http_requests/call_and_print.go
+++ http_requests/call_and_print.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "time"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 func main() {
 	netClient := &http.Client{
 		Timeout: time.Second * 10,
 	}
-	  
+
 	response, _ := netClient.Get("https://jsonplaceholder.typicode.com/users")
 	buf, _ := ioutil.ReadAll(response.Body)
-	fmt.Println(string(buf));
+	fmt.Println(string(buf))
 }
